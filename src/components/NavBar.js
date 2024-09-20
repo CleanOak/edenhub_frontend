427ad535c0958aaaaadcdb506afdb2ee73bbbb8a
Morgan Asare <ohenemorgan@gmail.com>
2024-09-20 08:38:04 +0000

    add navigation to the pages by using router-dom

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,20 +1,30 @@
 import React from 'react'
 import styles from "../styles/NavBar.module.css";
 import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { NavLink } from 'react-router-dom';
 
 
 const NavBar = () => {
   return (
     <Navbar collapseOnSelect expand="md" fixed='top' className={styles.NavBar}>
       <Container>
-        <Navbar.Brand className={styles.Brand}>EdenHub</Navbar.Brand>
+        <NavLink to="/">
+          <Navbar.Brand className={styles.Brand}>
+            EdenHub
+          </Navbar.Brand>
+        </NavLink>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link><i class="fa-solid fa-house"></i>Home</Nav.Link>
-            <Nav.Link><i class="fa-solid fa-right-to-bracket"></i>Sign in</Nav.Link> 
-            <Nav.Link><i class="fa-solid fa-user-plus"></i>Sign up</Nav.Link>  
+            <NavLink
+            exact
+            className={styles.NavLink} 
+            activeClassName={styles.Active} 
+            to="/"><i class="fa-solid fa-house"></i>Home</NavLink>
+            <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/signin"><i class="fa-solid fa-right-to-bracket"></i>Sign in</NavLink> 
+            <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/signup"><i class="fa-solid fa-user-plus"></i>Sign up</NavLink>  
           </Nav>
         </Navbar.Collapse>
       </Container>
