9bf07f026ec8bfec10f8d03832b85b099e321ef2
Morgan Asare <ohenemorgan@gmail.com>
2024-09-23 14:26:44 +0000

    add backend url to axios

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,5 +1,9 @@
 import axios from "axios";
 
-axios.defaults.baseURL = 'https://edenhub-060ed3b8a582.herokuapp.com/'
+axios.defaults.baseURL = 'https://edenhubbackend-1f7d010b86f9.herokuapp.com/'
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
 axios.defaults.withCredentials = true
+
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
