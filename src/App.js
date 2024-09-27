e808cfd06f0540f3d0149ee3c3cb8aa45d9eacff
Morgan Asare <ohenemorgan@gmail.com>
2024-09-27 12:46:43 +0000

    add post page and styling

--- src/App.js
+++ src/App.js
@@ -16,11 +16,11 @@ function App() {
       <NavBar/>
       <Container className={styles.Main}>
         <Switch>
-          <Route exact path="/" render={() => <h1>Home Page</h1>}/>
-          <Route exact path="/signin" render={() => <SignInForm/>}/>
-          <Route exact path="/signup" render={() => <SignUpForm/>}/>
-          <Route exact path="/posts/create" render={() => <PostCreateForm/>}/>
-          <Route exact path="posts/:id" render={() => <PostPage/>}/>
+          <Route exact path="/" render={() => <h1>Home Page</h1>} />
+          <Route exact path="/signin" render={() => <SignInForm/>} />
+          <Route exact path="/signup" render={() => <SignUpForm/>} />
+          <Route exact path="/posts/create" render={() => <PostCreateForm/>} />
+          <Route exact path="/posts/:id" render={() => <PostPage />} />
           <Route render={() => <p>Page not found!</p>} />
         </Switch>  
       </Container>
